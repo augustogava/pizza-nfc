c68225c6bb53b998e63ec9f8e9f38b42e6a4e782
augustogava <augustodj@gmail.com>
2025-01-19 19:37:56 -0300

    - news

--- src/app/actions/generateNotificationUrl.ts
+++
@@ -1,8 +0,0 @@
-'use server'
-
-export async function generateNotificationUrl(orderId: string, storeNumber: string) {
-  const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
-  const notificationUrl = `${baseUrl}/api/notify?orderId=${orderId}&storeNumber=${storeNumber}`
-  return notificationUrl
-}
-
